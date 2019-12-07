430d4ff51d8b1cdb054112b4dcf31fe1566b27ca
Taoudi <taoudi128@gmail.com>
2019-12-07 22:10:57 +0100

    Gaussian Process part 1 done!!!

--- Assignment 1/GaussianProcess.py
+++ Assignment 1/GaussianProcess.py
@@ -1,9 +1,13 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from mpl_toolkits.mplot3d import Axes3D
 
-tau = 1
+tau = 2
+noise = math.sqrt(3)
+inv_noise = 0
 
 
 def kernel(x, y):
@@ -30,12 +34,13 @@ def cov(x, x2):
 def data():
     xt = np.array([-4, -3, -2, -1, 0, 2, 3, 5])
     x = xt.reshape(-1, 1)
-    t = (2 + pow((0.5 * x - 1), 2) * np.sin(3 * x))
+    t = (2 + pow((0.5 * x - 1), 2) * np.sin(3 * x)) + np.random.normal(0, noise, x.shape)
+    print(t)
     return x, t
 
 
 def new_data():
-    return np.arange(-7, 7, 0.01).reshape(-1, 1)
+    return np.arange(-10, 10, 0.05).reshape(-1, 1)
 
 
 def gauss_it(mu, co):
@@ -50,14 +55,38 @@ def gauss_it(mu, co):
             arr = np.c_[arr, np.array(fff)]
         i += 1
     dt, t = data()
-    plt.scatter(dt, t,zorder=10)
+    plt.scatter(dt, t, zorder=10)
     lines = np.transpose(arr)
-    i=2
+    i = 2
     for line in lines:
-        print(line)
-        plt.plot(new_data(), line,zorder=i)
-        i+=1
-    plt.title("GP Posterior with l=1")
+        plt.plot(new_data(), line, zorder=i)
+        i += 1
+    plt.title("GP Posterior with l=" + str(tau))
+    plt.xlabel("x")
+    plt.ylabel("f")
+    plt.show()
+
+
+def gauss_it_mean(mu, co):
+    times = 10
+    i = 0
+    arr = 0
+    while i < times:
+        fff = np.random.multivariate_normal(mu.ravel(), co)
+        if i == 0:
+            arr = fff
+        else:
+            arr = np.c_[arr, np.array(fff)]
+        i += 1
+    dt, t = data()
+    plt.scatter(dt, t, zorder=10)
+    lines = np.transpose(arr)
+    meanline = lines.mean(axis=0)
+    plt.plot(new_data(), meanline, zorder=10)
+    for line in lines:
+        plt.fill_between(new_data().ravel(), meanline, line, color='grey', alpha='1')
+        i += 1
+    plt.title("GP Posterior with l=" + str(tau) + "Noise Parameter = " + str(inv_noise))
     plt.xlabel("x")
     plt.ylabel("f")
     plt.show()
@@ -86,12 +115,13 @@ x, t = data()
 x_new = new_data()
 
 f = sample_f(np.zeros(len(x)), cov(x, x))
+print(3 * np.identity(len(x)))
 
-inverse = np.linalg.inv(cov(x, x))
+inverse = np.linalg.inv(cov(x, x) + (inv_noise ** 2) * np.identity(len(x)))
+
 xnewx = np.transpose(cov(x_new, x))
-mu = xnewx @ inverse @ t
-print(mu)
+mult = xnewx @ inverse
+mu = mult @ t
 
-var = cov(x_new, x_new) - xnewx @ inverse @ np.transpose(cov(x, x_new))
-print(var)
-gauss_it(mu, var)
+var = cov(x_new, x_new) - mult @ np.transpose(cov(x, x_new))
+gauss_it_mean(mu, var)
