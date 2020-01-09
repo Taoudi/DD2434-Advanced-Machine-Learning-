c2e992aa79fc80901e9c92fb5b2848688a743600
Taoudi <taoudi128@gmail.com>
2020-01-09 21:41:38 +0100

    Extracting Document and Topics :thumbsup:

--- misc/TextExtraction.py
+++ misc/TextExtraction.py
@@ -6,17 +6,44 @@ current_path = dir_path + '/reuters21578'
 directory = os.fsencode(current_path)
 
 
+class Document:
+
+    def __init__(self, topics, text):
+        self.topics = topics
+        self.text = text
+
+    def __str__(self):
+        return str("TOPICS:" + "\n" + str(self.topics) + "\n" + self.text)
+
+
 def extract_text(file_name):
+    documents = []
     path = 'reuters21578/' + file_name
-    f = open(path, 'r')
+    f = open(path, 'rb')
     data = f.read()
     soup = BeautifulSoup(data)
-    contents = soup.findAll('body')
+    contents = soup.findAll('reuters')
     for content in contents:
-        print(content.text)
+        if content['topics'] != "NO":
+            topic = content.findAll('topics')
+            Ds = topic[0].findAll('d')
+            topics = []
+            for d in Ds:  ### Iterating through topics for specific document
+                topics.append(d.text)
 
+            text = ""
+            textBody = content.findAll('body')
+            if len(textBody) == 1:
+                text = textBody[0].text  ### The actual document (in textform, needs to be converted into BoW)
+                documents.append(Document(topics, text))
+    return documents
+
+
+documents = []
 
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".sgm"):
-        extract_text(filename)
+        documents += extract_text(filename)
+
+print(documents[1000])
